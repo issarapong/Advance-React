30562b959da9b087463968ff6b3529acbc4009e3
IS <1588186+issarapong@users.noreply.github.com>
2023-05-24 10:04:08 +0700

    simple state

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-#
+# Redux
 
-## Redux
-
+<img src="https://res.cloudinary.com/practicaldev/image/fetch/s--m5BdPzhS--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://i.imgur.com/riadAin.gif" alt="redux-flow" loading="lazy" width="880" height="660" data-animated="true" id="animated-0" class="ff-image">
+## Redux (single point of truth)
+- ไม่ต้องส่ง props
+- redux = context+Reducer
 ## Setup tailwind
+
+
+## 
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //STATE
+  const [count, setCount] = useState(0);  // set stat เริ่มต้น 0 เพื่อเตรียม + stat /จะสัมพันกับ type เช่น ถ้าเริ่มต้นเปน string ก็เอาไป + ไม่ได้
 
+  //LOGIC
+  const increase = () => setCount((p) => p + 1);
+  const decrease = () => setCount((p) => -1);
+  const reset = () => setCount(0);
+
+  //UI DISPLAY
   return (
-    <>
-     <h1 className='text-red-500'>Redux</h1>
-      
-    </>
-  )
+    <div>
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-2xl">Redux 😄</h1>
+        <h3>Count : {count}</h3>
+        <div className="flex gap-4">
+          <button
+            children
+            className="bg-slate-300 px-4 py-2 rouded-sm"
+            onClick={decrease}
+          >
+            -
+          </button>
+          <button
+            children
+            className="bg-slate-300 px-4 py-2 rouded-sm"
+            onClick={reset}
+          >
+            reset
+          </button>
+          <button
+            children
+            className="bg-slate-300 px-4 py-2 rouded-sm"
+            onClick={increase}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
